5a8e87ef8503b95b4dd88327fe1e5bb18c8209cd
Yahor Hryhoryk <91895255+xxxEGOxxx@users.noreply.github.com>
2023-11-24 19:41:33 +0100

    Add files via upload

---
+++ backend/src/main/java/com/ego/movies/Movie.java
@@ -0,0 +1,27 @@
+package com.ego.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    private List<Review> reviewIds;
+}
